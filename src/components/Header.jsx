1098cb5421c5244269db21b7705671ad5276a0e8
JuniorCostaDev <joel_br_junior@hotmail.com>
2022-09-12 02:46:18 -0300

    feat(api/route): cria contexto global exportando o data do usuário, rota de conta do usuário

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,29 @@
-import "./Header.scss"
-import {ReactComponent as Dogs} from "../Assets/dogs.svg"
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-
+import "./Header.scss";
+import { ReactComponent as Dogs } from "../Assets/dogs.svg";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from "../UserContext";
 
 const Header = () => {
+  const { data } = useContext(UserContext);
   return (
-    <div className='Header'>
-      <nav className='container'>
-        <Link className="logo" to="/" aria-label="Dogs - Home"><Dogs/></Link>
-        <Link className="login" to="/login">Login / Criar</Link>
+    <div className="Header">
+      <nav className="container">
+        <Link className="logo" to="/" aria-label="Dogs - Home">
+          <Dogs />
+        </Link>
+        {data ? (
+          <Link className="conta" to="/conta">
+            {data.nome}
+          </Link>
+        ) : (
+          <Link className="login" to="/login">
+            Login / Criar
+          </Link>
+        )}
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
