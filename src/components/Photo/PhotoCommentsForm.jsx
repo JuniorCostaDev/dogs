cbe754dc88cb30a2c001d2e7b0a798f2a59f4f30
JuniorCostaDev <joel_br_junior@hotmail.com>
2022-10-10 10:41:08 -0300

    feat: vizualição de comentários e função de comentar

---
+++ src/components/Photo/PhotoCommentsForm.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { COMMENT_POST } from "../../api";
+import { ReactComponent as Enviar } from "../../Assets/enviar.svg";
+import useFetch from "../../Hooks/useFetch";
+import Error from "../Helper/Error";
+import "./PhotoCommentsForm.scss";
+
+const PhotoCommentsForm = ({ id, setComments }) => {
+  const [comment, setComment] = useState("");
+  const { request, error } = useFetch();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { url, options } = COMMENT_POST(id, { comment });
+    const { response, json } = await request(url, options);
+    if (response.ok) {
+      setComment("");
+      setComments((comments) => [...comments, json]);
+    }
+  }
+
+  return (
+    <form className="PhotoCommentsForm" onSubmit={handleSubmit}>
+      <textarea
+        className="textarea"
+        id="comment"
+        name="comment"
+        placeholder="comente..."
+        value={comment}
+        onChange={({ target }) => setComment(target.value)}
+      />
+      <button>
+        <Enviar />
+      </button>
+      <Error error={error} />
+    </form>
+  );
+};
+
+export default PhotoCommentsForm;
