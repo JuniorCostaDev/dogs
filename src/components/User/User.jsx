88805ec20fa34c4fbc40d2dc7e1d9e99efd82920
JuniorCostaDev <joel_br_junior@hotmail.com>
2022-10-11 16:46:14 -0300

    feat: infiteScroll(feed.jsx e feedphotos.jsx)

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Feed from '../Feed/Feed'
-import UserHeader from './UserHeader'
-import UserPhotoPost from './UserPhotoPost'
-import UserStats from './UserStats'
+import React, { useContext } from "react";
+import { Route, Routes } from "react-router-dom";
+import { UserContext } from "../../UserContext";
+import Feed from "../Feed/Feed";
+import UserHeader from "./UserHeader";
+import UserPhotoPost from "./UserPhotoPost";
+import UserStats from "./UserStats";
 
 const User = () => {
+  const { data } = useContext(UserContext);
+
   return (
-    <section className='container'>
+    <section className="container">
       <UserHeader />
       <Routes>
-        <Route path='/' element={<Feed />}/>
-        <Route path='postar' element={<UserPhotoPost />}/>
-        <Route path='estatisticas' element={<UserStats />}/>
-
+        <Route path="/" element={<Feed user={data.id}/>} />
+        <Route path="postar" element={<UserPhotoPost />} />
+        <Route path="estatisticas" element={<UserStats />} />
       </Routes>
     </section>
-  )
-}
+  );
+};
 
-export default User
+export default User;
